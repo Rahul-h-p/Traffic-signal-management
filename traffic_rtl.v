ad8d8285d872277b78eb03e2674e4dc989c07c22
Rahul-h-p <114655079+Rahul-h-p@users.noreply.github.com>
2026-02-19 11:17:35 +0530

    Updated traffic_rtl.v with initial code

--- traffic_rtl.v
+++ traffic_rtl.v
@@ -1,1 +1,7 @@
-//-----------Traffic signal light---------------
+//-----------Traffic Control signal light---------------
+module Traffic Control(input reset,
+                       output reg [1:0] NS, EW, //NORTH-SOUTH and East-WEST traffic light (00: RED, 01:YELLOW, 10: GREEN)
+                       output reg RNS, GNS, YNS, 
+                       output reg REW, GEW, YEW,
+                       output reg clk);
+  parameter
