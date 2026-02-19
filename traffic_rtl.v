88dbbed03796c4c04486bfa86723c790d31f69c2
Rahul-h-p <114655079+Rahul-h-p@users.noreply.github.com>
2026-02-19 11:26:10 +0530

    Update traffic_rtl.v instantiated internal oscillator

--- traffic_rtl.v
+++ traffic_rtl.v
@@ -4,4 +4,10 @@ module Traffic Control(input reset,
                        output reg RNS, GNS, YNS, 
                        output reg REW, GEW, YEW,
                        output reg clk);
-  parameter
+  parameter CLOCK_DIVIDER=2080000;
+  reg [31:0] counter=0;
+  reg divided_clock;
+
+  //*******Internal Oscillator********
+  defparam OSCJ_inst.NOM_FREQ="2.08";
+  OSCJ OSCJ_inst(.STDBY(1'b0),
