45c9e5a442b2fa92676ca823ec6180258e19db20
Rahul-h-p <114655079+Rahul-h-p@users.noreply.github.com>
2026-02-19 19:00:47 +0530

    Update traffic_rtl.v

--- traffic_rtl.v
+++ traffic_rtl.v
@@ -1,13 +1,12 @@
 //-----------Traffic Control signal light---------------
 module Traffic Control(input reset,
-                       output reg [1:0] NS, EW, //NORTH-SOUTH and East-WEST traffic light (00: RED, 01:YELLOW, 10: GREEN)
+                       output reg [1:0] NS, EW, //NORTH-SOUTH and East-WEST traffic light (00: RED, 01:GREEN, 10: YELLOW)
                        output reg RNS, GNS, YNS, 
-                       output reg REW, GEW, YEW,
-                       output reg clk);
-  parameter CLOCK_DIVIDER=2080000;
-  reg [31:0] counter=0;
-  reg divided_clock;
-
+                       output reg REW, GEW, YEW
+                      );
+  
+wire clk;
   //*******Internal Oscillator********
-  defparam OSCJ_inst.NOM_FREQ="2.08";
-  OSCJ OSCJ_inst(.STDBY(1'b0),
+  OSCH #("2.08") osc_inst(.STDBY(1'b0),
+                          .OSC(clk),
+                          .SEDSTDBY());
